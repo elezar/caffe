7950ad19c5ff399396374494999d1d30038fc07f
Calvin Seward <calvin.seward@zalando.de>
2016-03-16 18:06:38 +0100

    inital start of the shop the look data layer

---
+++ src/caffe/layers/shop_the_look_data_layer.cpp
@@ -0,0 +1,80 @@
+#ifdef USE_OPENCV
+#include <opencv2/core/core.hpp>
+
+#include <fstream>  // NOLINT(readability/streams)
+#include <iostream>  // NOLINT(readability/streams)
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "caffe/data_transformer.hpp"
+#include "caffe/layers/base_data_layer.hpp"
+#include "caffe/layers/shop_the_look_data_layer.hpp"
+#include "caffe/util/benchmark.hpp"
+#include "caffe/util/io.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/util/rng.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+ShopTheLookDataLayer<Dtype>::~ShopTheLookDataLayer<Dtype>() {
+  this->StopInternalThread();
+}
+
+template <typename Dtype>
+void ImageDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  
+}
+
+// This function is called on prefetch thread
+template <typename Dtype>
+void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
+  CPUTimer batch_timer;
+  batch_timer.Start();
+  double read_time = 0;
+  double trans_time = 0;
+  CPUTimer timer;
+  CHECK(batch->data_.count());
+  CHECK(this->transformed_data_.count());
+  ImageDataParameter image_data_param = this->layer_param_.image_data_param();
+  const int batch_size = image_data_param.batch_size();
+  const int new_height = image_data_param.new_height();
+  const int new_width = image_data_param.new_width();
+  const bool is_color = image_data_param.is_color();
+  string root_folder = image_data_param.root_folder();
+  ShopTheLookDataParameter stl_param =
+      this->layer_param_.shop_the_look_data_param();
+  
+  vector<float> uniform_random(batch_size);
+  caffe_rng_uniform(batch_size, float(0), float(1), uniform_random.data());
+  int 
+  for (int item_id = 0; item_id < batch_size; ++item_id) {
+    if (uniform_random[item_id] < stl_param.positive_probability()) {
+      // positive example case
+    } else {
+      // negative example case
+    }
+  }
+}
+
+template <typename Dtype>
+void BasePrefetchingDataLayer<Dtype>::Forward_cpu(
+  const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  Batch<Dtype>* batch = prefetch_full_.pop("Data layer prefetch queue empty");
+  
+  // Reshape to loaded data.
+  top[0]->ReshapeLike(batch->data_);
+  // Copy the data
+  caffe_copy(batch->data_.count(), batch->data_.cpu_data(),
+             top[0]->mutable_cpu_data());
+  DLOG(INFO) << "Prefetch copied";
+  prefetch_free_.push(batch);
+}
+
+INSTANTIATE_CLASS(ShopTheLookDataLayer);
+REGISTER_LAYER_CLASS(ShopTheLookData);
+
+}  // namespace caffe
+#endif  // USE_OPENCV
